51b1f1ba10183e131d2c42d9f64d5ccdde33684a
ImAyrix <ImAyrix@gmail.com>
2023-12-31 17:43:45 +0330

    Regexes improved: function inputs regex

--- main.go
+++ main.go
@@ -299,7 +299,8 @@ func findParameter(link string) []string {
 		allParameter = append(allParameter, stringFormat...)
 
 		// Function input
-		funcInput := myRegex(`.*\(\s*["|']?([\w\-]+)["|']?\s*(\,\s*["|']?([\w\-]+)["|']?\s*)?(\,\s*["|']?([\w\-]+)["|']?\s*)?\)`, body, []int{1, 3, 5})
+		funcInput := myRegex(`.*\(\s*["|']?([\w\-]+)["|']?\s*(\,\s*["|']?([\w\-]+)["|']?\s*)?(\,\s*["|']?([\w\-]+)["|']?\s*)?(\,\s*["|']?([\w\-]+)["|']?\s*)?(\,\s*["|']?([\w\-]+)["|']?\s*)?(\,\s*["|']?([\w\-]+)["|']?\s*)?(\,\s*["|']?([\w\-]+)["|']?\s*)?(\,\s*["|']?([\w\-]+)["|']?\s*)?(\,\s*["|']?([\w\-]+)["|']?\s*)?(\,\s*["|']?([\w\-]+)["|']?\s*)?\)`,
+			body, []int{1, 3, 5, 7, 9, 11, 13, 15, 17, 19})
 		allParameter = append(allParameter, funcInput...)
 
 		// Path Input
